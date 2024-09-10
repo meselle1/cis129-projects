d226ed1ae38761fe820d6fae0d147d4030a64b44
meselle1 <meselle@mail.pima.edu>
2024-09-09 21:48:51 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+#Author: Manga Eselle
+#My First Python Lab in CIS129 using github
+#Prints story about Johnny
+print('Johhny goes to his first day of school')
+print('He is scared of getting his lunch money')
+print('So Johhny decides to lift weights')
+print('Johnny became the hulk')
