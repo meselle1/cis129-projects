8ba054566d9cf86b8b0f6ffde073f92462e40354
meselle1 <meselle@mail.pima.edu>
2024-09-16 21:41:53 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -3,13 +3,17 @@
 #  Define the prices
 coffee_price = 5
 muffin_price = 4
+bagel_price = 3
+donut_price = 6
 
 # Ask the customer for the number of coffee and muffins
 num_coffees = int(input("Enter the number of coffees: "))
 num_muffins = int(input("Enter the number of muffins: "))
+num_bagels = int(input("Enter the number of bagels: "))
+num_donuts = int(input("Enter the number of donuts: "))
 
 # Calculate the subtotal
-subtotal = (num_coffees * coffee_price) + (num_muffins * muffin_price)
+subtotal = (num_coffees * coffee_price) + (num_muffins * muffin_price) + (num_bagels * bagel_price) + (num_donuts * donut_price)
 
 # Calculate the tax (6%)
 tax = subtotal * 0.06
@@ -22,8 +26,12 @@ print("*********************************")
 print("My Coffee and Muffin Shop")
 print("Number of coffees bought?")
 print("1")
-print("Number of muffins bought")
+print("Number of muffins bought?")
 print("2")
+print("Number of bagels bought?")
+print("2")
+print("Number of donuts bought?")
+print("4")
 print("*********************************")
 print("")
 print("*********************************")
@@ -34,3 +42,6 @@ print(f"Subtotal: ${subtotal:.2f}")
 print(f"Tax (6%): ${tax:.2f}")
 print(f"Total: ${total:.2f}")
 print("*********************************")
+print("")
+print("Thank you!")
+
