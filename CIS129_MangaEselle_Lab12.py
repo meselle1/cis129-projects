8f1642b75b859332c848adef92369631536010aa
meselle1 <meselle@mail.pima.edu>
2024-11-18 22:00:05 -0700

    Create CIS129_MangaEselle_Lab12.py

---
+++ CIS129_MangaEselle_Lab12.py
@@ -0,0 +1,31 @@
+
+class Pet:
+    def __init__(self, name):
+        self.name 
+
+# declaring input variables 
+inputName = ""
+inputType = ""
+inputAge = 0
+
+# user input 
+inputName = input("Enter your pet's name: " )
+inputType = input("Enter youe pet type: ")
+inputAge = int(input("Enter your pet's age: "))
+
+class Animal: 
+    Pet = None 
+    def __init__(self, name, type, age):
+        self.name = name
+        self.type = type
+        self.age = age 
+        Animal.pet = self
+
+getName = (inputName)
+getType = (inputType)
+getAge = (inputAge)
+
+print("Your pet's name is: ",  inputName)
+print("Your pet's type is:", inputType)
+print("Your pet's age is: ", inputAge)
+
